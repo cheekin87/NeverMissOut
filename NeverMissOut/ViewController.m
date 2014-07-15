2f607142685687c0bedd55347a8d4ee6a84fb787
cheekin87@gmail.com <cheekin87@gmail.com>
2014-07-15 15:46:36 +0800

    Initial Commit

---
+++ NeverMissOut/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  NeverMissOut
+//
+//  Created by KEN on 7/15/14.
+//  Copyright (c) 2014 chee kin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
