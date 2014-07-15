dacfa61baaeb575faccecd6e5c4bcb48dfd777e1
cheekin87@gmail.com <cheekin87@gmail.com>
2014-07-15 16:58:13 +0800

    Create a csh method that used to lazily create an instance of CoreDataHelper class, the instance of the class will used to -Initialize a managed object model. -Initialize a persistence store coordinator with a persistence store based on the managed object model. -Initialize a managed object context based on the persistence store coordinator.

--- NeverMissOut/AppDelegate.m
+++ NeverMissOut/AppDelegate.m
@@ -7,8 +7,25 @@
 //
 
 #import "AppDelegate.h"
+#define debug 1
+
 
 @implementation AppDelegate
+
+-(CoreDataHelper*)cdh
+{
+    if (debug==1)
+    {
+        NSLog(@"Running %@ '%@'", self.class,NSStringFromSelector(_cmd));
+    }
+    
+    if (!_coreDataHelper)
+    {
+        _coreDataHelper = [CoreDataHelper new];
+        [_coreDataHelper setupCoreData];
+    }
+    return _coreDataHelper;
+}
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
@@ -26,6 +43,7 @@
 {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    [[self cdh]saveContext];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
@@ -41,6 +59,7 @@
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    [[self cdh]saveContext];
 }
 
 @end
